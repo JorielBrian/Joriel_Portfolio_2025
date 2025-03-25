d81a704c18815d060161e96928cb37749fce68e1
Joriel Brian Sudario <jorielsudario@gmail.com>
2025-03-25 22:41:24 +0800

    update 3/25/25

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,8 @@ import About from './Components/About'
 import Skills from './Components/Skills'
 import Qualifications from './Components/Qualifications'
 import Footer from './Components/Footer'
-import { useRef } from 'react'
 
 function App() {
-  const aboutRef = useRef<HTMLDivElement | null>(null);
   return (
     <div>
       <Navigation/>
