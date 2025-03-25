0940ddecb417dd62cb86593ae67b886d6b94e9cf
Joriel Brian Sudario <jorielsudario@gmail.com>
2025-03-26 00:01:22 +0800

    remove stack

--- src/Components/Welcome.tsx
+++ src/Components/Welcome.tsx
@@ -6,7 +6,7 @@ const Welcome = () =>{
             <motion.div initial={{x:-100, opacity:0}} whileInView={{x:0, opacity:1}} transition={{ease:"easeIn", duration:1.5}}  className="justify-items-start m-5 font-sans">
                 <h1 className="text-4xl 2xl:text-8xl">Joriel Brian Sudario</h1>
                 <h2 className="text-2xl 2xl:text-6xl bg-gradient-to-r from-neutral-400 via-cyan-500 to-blue-800 bg-clip-text text-transparent">Junior Developer</h2>
-                <p className="text-md 2xl:text-2xl">React, Typescript, TailwindCSS</p>
+                {/* <p className="text-md 2xl:text-2xl">React, Typescript, TailwindCSS</p> */}
             </motion.div>
             <motion.div initial={{x:100, opacity:0}} whileInView={{x:0, opacity:1}} transition={{ease:"easeIn", duration:1.5}} id="myImage">
                 <img src="/Profile Picture.jpg" alt="My Image" className="m-auto size-50 my-5 2xl:size-120 rounded-full"/>
