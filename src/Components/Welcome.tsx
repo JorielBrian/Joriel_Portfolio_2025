adf8ed106712f3e87c21af52df725b68d0c8b92a
Joriel Brian Sudario <jorielsudario@gmail.com>
2025-04-14 21:46:28 +0800

    Update Welcome and About

--- src/Components/Welcome.tsx
+++ src/Components/Welcome.tsx
@@ -11,7 +11,7 @@ const Welcome = () =>{
             <div className="h-screen grid grid-cols-8 grid-rows-6 px-10">
                 <motion.div initial={{x:-100, opacity:0}} whileInView={{x:0, opacity:1}} transition={{ease:"easeIn", duration:1.5}}  className="col-start-1 col-end-9 xl:col-end-6 row-start-1 row-end-3 lg:row-end-3 font-sans">
                     <h1 className="text-3xl sm:text-6xl md:text-7xl 2xl:text-8xl">Joriel Brian Sudario</h1>
-                    <h2 className="text-xl h-14 border-amber-50 sm:text-5xl 2xl:text-6xl bg-gradient-to-r from-neutral-400 via-cyan-500 to-blue-800 bg-clip-text text-transparent"><Typewriter 
+                    <h2 className="text-xl pb-5 border-amber-50 sm:text-5xl 2xl:text-6xl bg-gradient-to-r from-neutral-400 via-cyan-500 to-blue-800 bg-clip-text text-transparent"><Typewriter 
                     words={['Junior Developer', 'IT Specialist', 'Computer Programmer', 'Circuit Assembler']}
                     loop = {true}
                     cursor cursorStyle = {'|'} cursorBlinking = {true} cursorColor="white"
@@ -21,11 +21,11 @@ const Welcome = () =>{
                     <img src="/Profile Picture.jpg" alt="My Image" className="m-auto my-5 size-80 sm:size-100 xl:size-115 2xl:size-120 rounded-full"/>
                 </motion.div>
                 <motion.div initial={{y:100, opacity:0}} whileInView={{y:0, opacity:1}} transition={{ease:"easeIn", duration:1.5}} className="col-start-1 col-end-9 py-4 md:col-end-5 xl:col-end-4 row-start-5 md:row-start-3 row-end-7 font-bold">
-                    <p className="text-center text-sm sm:text-md md:text-start font-sans">
-                        I'm Joriel Brian, a Junior Developer who is passionate, dedicated, hard-working, God-driven person and eager to learn.
+                    <p className="text-center text-sm sm:pt-15 md:p-0 sm:text-md md:text-start font-sans">
+                        I'm Joriel Brian, a Junior Developer / IT Specialist / Computer Programmer / Circuit Assembler who is passionate, dedicated, hard-working, God-driven person and eager to learn.
                     </p>
                     <p className="text-center text-sm sm:text-md md:text-start font-sans">
-                        Dream to be a future Senior Full Stack Web Developer
+                        Dream to be a future Senior Full Stack Web Developer or a Senior IT Specialist
                     </p>
                 </motion.div>
             </div>
