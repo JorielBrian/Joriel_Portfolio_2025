4773a3787cb58d915ae98b34439aa560b8b60ade
Joriel Brian Sudario <jorielsudario@gmail.com>
2025-10-20 23:30:30 +0800

    Update About and Welcome page

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -5,9 +5,22 @@ const About = () =>{
     return (
         <>
         <motion.h1 id="about" initial={{opacity:0}} whileInView={{opacity:1}} transition={{ease:"easeIn", duration:0.5}}  className="text-4xl text-center font-bold">About Me</motion.h1>
-        <div className="xl:flex w-4/5 mx-auto my-5 p-5 bg-sky-900/50 rounded-2xl content-center">
-            <motion.img src="/aboutmeimage.jpg" alt="aboutme" initial={{x:-100, opacity:0}} whileInView={{x:0, opacity:1}} transition={{ease:"easeIn", duration:0.5}} className="rounded-2xl xl:w-120 2xl:w-150 m-auto"/>
-            <motion.p initial={{x:100, opacity:0}} whileInView={{x:0, opacity:1}} transition={{ease:"easeIn", duration:0.5}}  className="p-3">{ABOUT && ABOUT}</motion.p>
+        <div className="relative w-4/5 mx-auto my-5 p-5 bg-[url(/aboutmeimage.jpg)] bg-cover bg-center rounded-2xl place-content-start overflow-hidden">
+            {/* <span className="absolute w-full">
+                <motion.img 
+                    src="/aboutmeimage.jpg" 
+                    alt="aboutme" 
+                    initial={{x:-100, opacity:0}} 
+                    whileInView={{x:0, opacity:1}} 
+                    transition={{ease:"easeIn", duration:0.5}} 
+                    className=" rounded-2xl xl:w-120 2xl:w-full m-auto justify-self-start" 
+                />
+            </span> */}
+            <div className="relative bg-sky-900/80 rounded-2xl">
+                { ABOUT && ABOUT.map(about => (
+                    <motion.p initial={{x:100, opacity:0}} whileInView={{x:0, opacity:1}} transition={{ease:"easeIn", duration:0.5}}  className="p-3">{about}</motion.p>
+                )) }
+            </div>    
         </div>
         </>
     );
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-export const ABOUT = `My biggest dream is to be a Full-Stack Web Developer or an IT Specialist. I am a passionate, dedicated, hard-working, and God-driven person that is eager to learn the things that I need to grow and improve to be a successful developer . Web development and circuit assembling is always my interest. As my journey in the industry continues, I become passionate and eager to learn more things, as we all know the technology and learning never stop. Outside of work, I am a musician in our local church as a lead guitarist, and being part of the Music and Arts Ministry, I also play a role as a guitar tech and a music theory tutor. `
+export const INTRODUCTION = [
+    "I'm Joriel Brian, a passionate and dedicated Web Developer, IT Specialist, and Computer Programmer with a strong foundation in full-stack development and hardware integration.",
+    "Driven by faith and a relentless work ethic, I'm constantly expanding my skills to become a future Senior Full-Stack Developer or Senior IT Specialist."
+];
+
+export const ABOUT = [
+    "I am a Full-Stack Developer with experience building end-to-end web applications using Laravel, Next.js, and PHP, with a focus on front-end development, database management, middleware, and routing. My front-end expertise includes JavaScript, TypeScript, Livewire, Flux UI, Tailwind CSS, and React.js, while on the back-end and database layer, I have worked with Prisma, GraphQL, Eloquent ORM, and SQL. I also utilize Git and GitHub for version control across projects.",
+    "I've led teams, created systems from scratch, and ensured projects are delivered on time with high quality. Throughout my career, I've served as a Systems Engineer II, Project Lead Developer, Systems Analyst, IT Officer, and Computer Programmer II, which has shaped me into a flexible and reliable developer who enjoys solving real-world problems with technology.",
+    "My primary career goal is to excel as a Full-Stack Web Developer or IT Specialist. I am a passionate, dedicated, hard-working, and faith-driven individual who is constantly eager to learn and grow professionally. Web development and circuit assembly have always been my core interests. As my journey in the tech industry continues, I remain passionate about expanding my knowledge, recognizing that technology and learning are endless pursuits.",
+    "Outside of coding, spending time with my family is my greatest source of joy and strength—it helps me recharge emotionally and stay grounded. I also serve as a Lead Guitarist at Hills of Glory Mabalacat, where I express my passion for music and worship. Additionally, I find peace and encouragement in fellowship with my disciples and churchmates, growing together through God's Word."
+];
+
 export const CONTACTS = [
     {type:"email", contact:"jorielsudario@gmail.com", icon:"/mail.png"},
     {type:"phone", contact:"+63 965 447 5177", icon:"/phone.png"}
