07fd8883b52088442245ba81e3d531303791dd6c
Joriel Brian Sudario <jorielsudario@gmail.com>
2025-12-21 14:32:58 +0800

    added a file for my primary core skills

---
+++ src/data/core_skills.ts
@@ -0,0 +1,8 @@
+export const CORE_SKILLS = [
+    {skill:"Next.js", image: "/next.png", proficiency:"Intermediate"},
+    {skill:"TypeScript", image: "/typescript.png", proficiency:"Intermediate"},
+    {skill:"Tailwind CSS", image: "/tailwind.png", proficiency:"Advanced"},
+    {skill:"Prisma", image: "/prisma.png", proficiency:"Functional"},
+    {skill:"SQL", image: "/sql.png", proficiency:"Intermediate"},
+    {skill:"GIT", image: "/git.png", proficiency:"Advanced"},
+]
