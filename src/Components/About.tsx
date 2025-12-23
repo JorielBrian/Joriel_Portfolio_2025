38aaf9a7cd48efd536a6d0a14f6e81f520126762
Joriel Brian Sudario <jorielsudario@gmail.com>
2025-12-24 00:05:43 +0800

    made the About section responsive

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -16,11 +16,11 @@ const About = () =>{
                     className=" rounded-2xl xl:w-120 2xl:w-full m-auto justify-self-start" 
                 />
             </span> */}
-            <div className="relative p-5 grid grid-cols-3 align-middle bg-sky-900/80 rounded-2xl">
-                <motion.div initial={{x:100, opacity:0}} whileInView={{x:0, opacity:1}} transition={{ease:"easeIn", duration:1.5}} id="myImage" className="h-fit">
+            <div className="relative p-5 grid 2xl:grid-cols-3 align-middle bg-sky-900/80 rounded-2xl">
+                <motion.div initial={{x:100, opacity:0}} whileInView={{x:0, opacity:1}} transition={{ease:"easeIn", duration:1.5}} id="myImage" className="hidden 2xl:block h-fit">
                     <img src="/Profile Picture.png" alt="My Image" className="m-auto my-5 rounded-full"/>
                 </motion.div>
-                <div className="col-span-2 content-center">
+                <div className="2xl:col-span-2 content-center">
                     <div className="p-5 text-white font-sans">
                         { ABOUT && ABOUT.map(about => (
                             <motion.p initial={{x:100, opacity:0}} whileInView={{x:0, opacity:1}} transition={{ease:"easeIn", duration:0.5}}  className="p-3 text-xl">{about}</motion.p>
