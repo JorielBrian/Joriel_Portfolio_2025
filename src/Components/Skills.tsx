738eae8499f09dc0cb31f714f3b5b0eec76bde1b
Joriel Brian Sudario <jorielsudario@gmail.com>
2025-12-24 00:28:27 +0800

    made the padding of each skill image responsive

--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -29,7 +29,7 @@ const Skills = () =>{
                             {area.skills.map(skill => (
                                 <div key={skill.skill}>
                                     <motion.div animate={{y:[10, -10]}} transition={{ease:"linear", duration:2, repeat: Infinity, repeatType:"reverse"}}  className="relative p-3 justify-center">
-                                        <motion.img initial={{opacity:0}} whileInView={{opacity:1}} transition={{ease:"easeIn", duration:1.5}} src={skill.image} alt={skill.skill} className="size-12 sm:size-15 md:size-20 lg:size-22 xl:size-25 2xl:size-30 rounded-xl 2xl:p-3 bg-gray-500/75 2xl:rounded-4xl hover:animate-bounce m-auto"/>
+                                        <motion.img initial={{opacity:0}} whileInView={{opacity:1}} transition={{ease:"easeIn", duration:1.5}} src={skill.image} alt={skill.skill} className="size-12 sm:size-15 md:size-20 lg:size-22 xl:size-25 2xl:size-30 rounded-xl p-1 md:p-2 2xl:p-3 bg-gray-500/75 2xl:rounded-4xl hover:animate-bounce m-auto"/>
                                         <h1 className="text-center font-bold text-amber-200 text-[11px] md:text-sm xl:text-lg">{skillrate(skill.proficiency)}</h1>
                                         <h1 className="text-center font-bold text-cyan-500 text-[11px] md:text-sm xl:text-lg">{skill.skill}</h1>
                                     </motion.div>
