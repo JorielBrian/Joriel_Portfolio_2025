d75e698fde98c67aae8a23e98237263dc88d490e
Joriel Brian Sudario <jorielsudario@gmail.com>
2025-12-24 00:25:17 +0800

    resize the text for responsive

--- src/Components/Welcome.tsx
+++ src/Components/Welcome.tsx
@@ -12,20 +12,20 @@ const Welcome = () =>{
         <div id="home" className="w-full mt-30 ml-0">
             <div className="h-screen px-10 mb-20 xl:mb-0">
                 <motion.div initial={{x:-100, opacity:0}} whileInView={{x:0, opacity:1}} transition={{ease:"easeIn", duration:1.5}}  className="text-center font-sans">
-                    <h1 className="text-3xl font-bold sm:text-6xl md:text-7xl 2xl:text-9xl">Joriel Brian Sudario</h1>
-                    <h2 className="text-xl pb-5 border-amber-50 sm:text-5xl 2xl:text-8xl bg-gradient-to-r from-neutral-400 via-cyan-500 to-blue-800 bg-clip-text text-transparent"><Typewriter 
+                    <h1 className="text-3xl font-bold sm:text-8xl xl:text-9xl">Joriel Brian Sudario</h1>
+                    <h2 className="text-xl pb-5 border-amber-50 sm:text-5xl lg:text-7xl 2xl:text-8xl bg-gradient-to-r from-neutral-400 via-cyan-500 to-blue-800 bg-clip-text text-transparent"><Typewriter 
                     words={['Web Developer', 'Full Stack Developer', 'System Engineer', 'Software Engineer', 'Computer Programmer', 'IT Specialist', 'Circuit Assembler', 'Information Technology']}
                     loop = {true}
                     cursor cursorStyle = {'|'} cursorBlinking = {true} cursorColor="white"
                     /></h2>
                     {  CORE_SKILLS && CORE_SKILLS.map(skill => (
-                        <img key={skill.skill} src={skill.image} alt={skill.skill} className="inline size-10 p-2 sm:size-12 md:size-14 lg:size-14 xl:size-16 2xl:size-25 m-1 rounded-xl hover:animate-bounce"/>
+                        <img key={skill.skill} src={skill.image} alt={skill.skill} className="inline size-14 xl:size-20  2xl:size-25 m-1 rounded-xl hover:animate-bounce"/>
                     )) }
                 </motion.div>
                 <motion.div initial={{y:100, opacity:0}} whileInView={{y:0, opacity:1}} transition={{ease:"easeIn", duration:1.5}} className="py-4">
-                        <div className="justify-items-center">
+                        <div className="justify-center space-y-2 text-center">
                             { INTRODUCTION && INTRODUCTION.map(intro => (
-                                <p className="text-sm w-fit sm:pt-15 md:p-0 sm:text-xl md:text-start font-sans">{ intro }</p>
+                                <p key={intro} className="w-fit justify-items-center text-lg lg:text-xl xl:text-2xl 2xl:text-3xl sm:pt-15 md:p-0 md:text-start font-sans">{ intro }</p>
                             )) }
                         </div>
                 </motion.div>
