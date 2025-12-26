8c275836b1a847bf4ebbe03969e6126f598e7b3b
Joriel Brian Sudario <jorielsudario@gmail.com>
2025-12-26 11:08:17 +0800

    enhanced header

--- src/Components/Sections/Navigation.tsx
+++ src/Components/Sections/Navigation.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { House, BriefcaseBusiness, Folder } from "lucide-react";
 import { useEffect, useState } from "react";
 
@@ -22,32 +22,90 @@ const Navigation = () =>{
 
     return (
         <motion.section className={scroll ? "navbar" : "flex w-full sm:w-2/3 justify-center"}>
-            <div className="flex w-full !py-2 px-5 rounded-sm justify-between">
+            <div className="flex w-full !py-0 px-5 rounded-sm justify-between">
+                {/* Logo */}
                 <div className="w-fit !py-0">
                     <img 
                         src="/logo.png" 
                         alt="JB logo" 
-                        className="size-14 md:size-16 lg:size-18 xl:size-20 2xl:size-22 lg:p-2"
+                        className="size-14 md:size-16 p-1"
                     />
                 </div>
 
+                {/* Navigation */}
                 <div className="flex items-center !py-0">
                     {/* MOBILE VIEW */}
-                    <ul className="flex lg:hidden relative py-auto items-center text-md md:text-lg xl:text-xl *:w-fit *:h-10 *:m-1 *:p-3 *:text-center *:rounded-xl *:hover:scale-110 *:active:bg-blue-950 *:active:scale-90">
-                        <li><Link to='/'><House /></Link></li>
-                        <li><Link to='/experience'><BriefcaseBusiness /></Link></li>
-                        <li><Link to='/project'><Folder /></Link></li>
+                    <ul className="flex lg:hidden">
+                        <li>
+                            <NavLink 
+                                to='/' 
+                                className={({ isActive }) => 
+                                    `navButton ${isActive ? 'active' : ''}`
+                                }
+                            >
+                                <House />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink 
+                                to='/experience' 
+                                className={({ isActive }) => 
+                                    `navButton ${isActive ? 'active' : ''}`
+                                }
+                            >
+                                <BriefcaseBusiness />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink 
+                                to='/project' 
+                                className={({ isActive }) => 
+                                    `navButton ${isActive ? 'active' : ''}`
+                                }
+                            >
+                                <Folder />
+                            </NavLink>
+                        </li>
                     </ul>
                     {/* DESKTOP VIEW */}
-                    <ul className="hidden lg:flex relative py-auto items-center text-md md:text-lg xl:text-md *:w-fit *:h-10 *:m-1 *:p-3 *:text-center *:rounded-xl *:hover:scale-110 *:active:bg-blue-950 *:active:scale-90">
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/experience'>Experiences</Link></li>
-                        <li><Link to='/project'>Projects</Link></li>
+                    <ul className="hidden lg:flex">
+                        <li>
+                            <NavLink 
+                                to='/' 
+                                className={({ isActive }) => 
+                                    `navButton ${isActive ? 'active' : ''}`
+                                }
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink 
+                                to='/experience' 
+                                className={({ isActive }) => 
+                                    `navButton ${isActive ? 'active' : ''}`
+                                }
+                            >
+                                Experiences
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink 
+                                to='/project' 
+                                className={({ isActive }) => 
+                                    `navButton ${isActive ? 'active' : ''}`
+                                }
+                            >
+                                Projects
+                            </NavLink>
+                        </li>
                     </ul>
                 </div>
+
+                {/* CV Button */}
                 <div className="flex items-center !py-0">
-                    <a href="Joriel Brian Sudario - CV.pdf" target="_blank"><button className="flex lg:hidden px-3 py-1 bg-blue-700/60 rounded-full border-2 border-blue-600/80">CV</button></a>
-                    <a href="Joriel Brian Sudario - CV.pdf" target="_blank"><button className="hidden lg:flex px-3 py-1 bg-blue-700/60 rounded-full border-2 border-blue-600/80">Resume</button></a>
+                    <a href="Joriel Brian Sudario - CV.pdf" target="_blank"><button className="navbarCV flex lg:hidden">CV</button></a>
+                    <a href="Joriel Brian Sudario - CV.pdf" target="_blank"><button className="navbarCV hidden lg:flex">Resume</button></a>
                 </div>
             </div>
         </motion.section>
