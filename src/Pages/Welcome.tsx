815befbf236361df00117ecb466c1cdbc66f3efb
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-01-03 06:34:52 +0800

    separated the hero section in welcome page

--- src/Pages/Welcome.tsx
+++ src/Pages/Welcome.tsx
@@ -1,38 +1,16 @@
-import { motion } from "framer-motion"
-import { INTRODUCTION } from "../data/index";
+
+import Hero from "../Components/Sections/Hero";
 import About from "../Components/Sections/About";
 import Skills from "../Components/Sections/Skills";
-import { Typewriter} from "react-simple-typewriter";
-import { SKILLS } from "../data/all_skills";
 
 const Welcome = () =>{
     window.scrollTo(0, 0);
 
     return (
         <div id="home" className="w-full mt-10 ml-0">
-            <div className="h-screen px-10 mb-20 xl:mb-0">
-                <motion.div initial={{y:100, opacity:0}} whileInView={{y:0, opacity:1}} transition={{ease:"easeIn", duration:1.5}}  className="text-center font-sans">
-                    <h1 className="text-6xl font-bold sm:text-7xl md:text-8xl xl:text-9xl mb-2 tracking-tight">Joriel Brian Sudario</h1>
-                    <h3 className="pb-5 border-amber-50 text-2xl sm:text-4xl md:text-5xl lg:text-7xl 2xl:text-8xl bg-gradient-to-r from-neutral-400 via-cyan-500 to-blue-800 bg-clip-text text-transparent"><Typewriter 
-                    // words={['Web Developer', 'Full Stack Developer', 'System Engineer', 'Software Engineer', 'Computer Programmer', 'IT Specialist', 'Circuit Assembler', 'Information Technology']}
-                    words={['Front-end Developer', 'Web Developer', 'Computer Programmer']}
-                    loop = {true}
-                    cursor cursorStyle = {'|'} cursorBlinking = {true} cursorColor="white"
-                    /></h3>
-                    { SKILLS && SKILLS.filter(skill => skill.focus === true).map(skill => (
-                        <img key={skill.skill} src={skill.image} alt={skill.skill} title={skill.skill} className="inline size-6 sm:size-8 md:size-10 lg:size-12 xl:size-14 2xl:size-16 m-1 rounded-md hover:animate-pulse"/>
-                    )) }
-                </motion.div>
-                <motion.div initial={{y:-100, opacity:0}} whileInView={{y:0, opacity:1}} transition={{ease:"easeIn", duration:1.5}} className="py-4 mt-10">
-                        <div className="text-center space-y-2">
-                            { INTRODUCTION && INTRODUCTION.map(intro => (
-                                <p key={intro} className="font-sans">{ intro }</p>
-                            )) }
-                        </div>
-                </motion.div>
-            </div>
-            <Skills></Skills>
-            <About></About>
+            <Hero />
+            <Skills />
+            <About />
         </div>
     );
 }
